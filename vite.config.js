c31082aca56fa75a9198a799af89fb95569f8be4
tkp-8 <tomkpilgram@icloud.com>
2024-11-21 20:40:14 +0900

    .

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,14 @@
-import { defineConfig } from 'vite';
-import laravel from 'laravel-vite-plugin';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from "vite";
+import laravel from "laravel-vite-plugin";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
+    scripts: {
+        build: "vite build",
+    },
     plugins: [
         laravel({
-            input: 'resources/js/app.jsx',
+            input: "resources/js/app.jsx",
             refresh: true,
         }),
         react(),
