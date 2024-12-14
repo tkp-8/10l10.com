990be79eaa2a08f91814ba045785651b43387b17
tkp-8 <tomkpilgram@icloud.com>
2024-12-14 16:07:55 +0900

    .

--- resources/js/Pages/Home.jsx
+++ resources/js/Pages/Home.jsx
@@ -1,10 +1,18 @@
+import { Button, buttonVariants } from "@/components/ui/button";
+import { Head, Link } from "@inertiajs/react";
 export default function Home() {
   return (
     <>
       <div className="notch"></div>
-      <div className="p-2 h-svh grid grid-cols-5 grid-rows-3">
-        <div className="bg-grain-blue w-full h-full bg-cover p-1 rounded-md"></div>
-        <button className="btn-blue">click here</button>
+      <div className="p-2 h-svh">
+        <div className="bg-grain-blue w-full h-full bg-cover p-1 rounded-md grid grid-cols-5 grid-rows-3">
+          <Link
+            className={buttonVariants({ variant: "outline" })}
+            href={route("dashboard")}
+          >
+            j
+          </Link>
+        </div>
       </div>
     </>
   );
